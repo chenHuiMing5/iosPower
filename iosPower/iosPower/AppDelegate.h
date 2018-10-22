cdb590afad8c19b5e45d8f89fb835c4742db49c8
ngmmxh <chenhuiming@nicomama.com>
2018-10-22 18:49:19 +0800

    -n 第一次提交

---
+++ iosPower/iosPower/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iosPower
+//
+//  Created by ngmmxh on 2018/10/22.
+//  Copyright © 2018年 ngmmxh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iosPower/iosPower/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iosPower
+//
+//  Created by ngmmxh on 2018/10/22.
+//  Copyright © 2018年 ngmmxh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
